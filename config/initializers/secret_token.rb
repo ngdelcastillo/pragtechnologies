380e701f6c954cb1699155a6ad8ee113d1ca1d6e
Noel del Castillo <ngdelcastillo@gmail.com>
2012-04-05 11:11:12 +0800

    Initial stage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pragtechnologies::Application.config.secret_token = '0e8d7b0f06345a314bba71725dd997c8c71ba55cbec501093286ca896e85dd071e4146312990f6f197d402ce43df812c2a611062e624414cc333c3630b62b3e7'
